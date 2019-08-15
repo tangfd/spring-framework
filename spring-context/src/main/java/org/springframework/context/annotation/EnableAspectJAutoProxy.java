b6ca7d2985be33e4892ddbd0ee07b970ac4a425b
tangfd <tangfd@foxmail.com>
2019-08-15 23:31:22 +0800

    TestAop

--- spring-context/src/main/java/org/springframework/context/annotation/EnableAspectJAutoProxy.java
+++ spring-context/src/main/java/org/springframework/context/annotation/EnableAspectJAutoProxy.java
@@ -16,105 +16,59 @@
 
 package org.springframework.context.annotation;
 
-import java.lang.annotation.Documented;
-import java.lang.annotation.ElementType;
-import java.lang.annotation.Retention;
-import java.lang.annotation.RetentionPolicy;
-import java.lang.annotation.Target;
+import org.springframework.aop.aspectj.annotation.AnnotationAwareAspectJAutoProxyCreator;
+import org.springframework.aop.aspectj.autoproxy.AspectJAwareAdvisorAutoProxyCreator;
+import org.springframework.aop.config.AopConfigUtils;
+import org.springframework.aop.framework.autoproxy.AbstractAdvisorAutoProxyCreator;
+import org.springframework.aop.framework.autoproxy.AbstractAutoProxyCreator;
+import org.springframework.beans.factory.config.BeanPostProcessor;
+import org.springframework.beans.factory.config.InstantiationAwareBeanPostProcessor;
+import org.springframework.beans.factory.config.SmartInstantiationAwareBeanPostProcessor;
+import org.springframework.beans.factory.support.BeanDefinitionRegistryPostProcessor;
+
+import java.lang.annotation.*;
 
 /**
- * Enables support for handling components marked with AspectJ's {@code @Aspect} annotation,
- * similar to functionality found in Spring's {@code <aop:aspectj-autoproxy>} XML element.
- * To be used on @{@link Configuration} classes as follows:
- *
- * <pre class="code">
- * &#064;Configuration
- * &#064;EnableAspectJAutoProxy
- * public class AppConfig {
- *
- *     &#064;Bean
- *     public FooService fooService() {
- *         return new FooService();
- *     }
- *
- *     &#064;Bean
- *     public MyAspect myAspect() {
- *         return new MyAspect();
- *     }
- * }</pre>
- *
- * Where {@code FooService} is a typical POJO component and {@code MyAspect} is an
- * {@code @Aspect}-style aspect:
- *
- * <pre class="code">
- * public class FooService {
- *
- *     // various methods
- * }</pre>
- *
- * <pre class="code">
- * &#064;Aspect
- * public class MyAspect {
- *
- *     &#064;Before("execution(* FooService+.*(..))")
- *     public void advice() {
- *         // advise FooService methods as appropriate
- *     }
- * }</pre>
- *
- * In the scenario above, {@code @EnableAspectJAutoProxy} ensures that {@code MyAspect}
- * will be properly processed and that {@code FooService} will be proxied mixing in the
- * advice that it contributes.
- *
- * <p>Users can control the type of proxy that gets created for {@code FooService} using
- * the {@link #proxyTargetClass()} attribute. The following enables CGLIB-style 'subclass'
- * proxies as opposed to the default interface-based JDK proxy approach.
- *
- * <pre class="code">
- * &#064;Configuration
- * &#064;EnableAspectJAutoProxy(proxyTargetClass=true)
- * public class AppConfig {
- *     // ...
- * }</pre>
- *
- * <p>Note that {@code @Aspect} beans may be component-scanned like any other.
- * Simply mark the aspect with both {@code @Aspect} and {@code @Component}:
- *
- * <pre class="code">
- * package com.foo;
- *
- * &#064;Component
- * public class FooService { ... }
- *
- * &#064;Aspect
- * &#064;Component
- * public class MyAspect { ... }</pre>
- *
- * Then use the @{@link ComponentScan} annotation to pick both up:
- *
- * <pre class="code">
- * &#064;Configuration
- * &#064;ComponentScan("com.foo")
- * &#064;EnableAspectJAutoProxy
- * public class AppConfig {
- *
- *     // no explicit &#064Bean definitions required
- * }</pre>
- *
- * <b>Note: {@code @EnableAspectJAutoProxy} applies to its local application context only,
- * allowing for selective proxying of beans at different levels.</b> Please redeclare
- * {@code @EnableAspectJAutoProxy} in each individual context, e.g. the common root web
- * application context and any separate {@code DispatcherServlet} application contexts,
- * if you need to apply its behavior at multiple levels.
- *
- * <p>This feature requires the presence of {@code aspectjweaver} on the classpath.
- * While that dependency is optional for {@code spring-aop} in general, it is required
- * for {@code @EnableAspectJAutoProxy} and its underlying facilities.
- *
- * @author Chris Beams
- * @author Juergen Hoeller
- * @since 3.1
- * @see org.aspectj.lang.annotation.Aspect
+ * 1. {@link EnableAspectJAutoProxy @EnableAspectJAutoProxy}功能解析
+ * <ul>
+ * <li>1. 通过{@link Import @Import}注解向容器中注入{@link AspectJAutoProxyRegistrar}组件</li>
+ * <li>2. {@link AspectJAutoProxyRegistrar} 是一个 {@link ImportBeanDefinitionRegistrar}，
+ * spring容器在执行{@link BeanDefinitionRegistryPostProcessor#postProcessBeanDefinitionRegistry}方法时，
+ * 进行调用{@link ImportBeanDefinitionRegistrar#registerBeanDefinitions}方法
+ * 实际执行的为（{@link ConfigurationClassPostProcessor#postProcessBeanDefinitionRegistry}），
+ * 并向spring容器中注册{@link AnnotationAwareAspectJAutoProxyCreator}组件，
+ * 它实际是一个{@link InstantiationAwareBeanPostProcessor},在bean创建之前尝试获取代理对象进行返回，
+ * bean名称为：{@link AopConfigUtils#AUTO_PROXY_CREATOR_BEAN_NAME}</li>
+ * </ul>
+ * 2. {@link AnnotationAwareAspectJAutoProxyCreator} 功能解析
+ * <p>
+ * 继承关系：
+ * {@link AnnotationAwareAspectJAutoProxyCreator}[initBeanFactory]
+ * <p>
+ * > {@link AspectJAwareAdvisorAutoProxyCreator}
+ * <p>
+ * > {@link AbstractAdvisorAutoProxyCreator}[setBeanFactory --> initBeanFactory]
+ * <p>
+ * > {@link AbstractAutoProxyCreator}[setBeanFactory,postProcessBeforeInstantiation,postProcessAfterInitialization]
+ * <p>
+ * > {@link SmartInstantiationAwareBeanPostProcessor}
+ * <p>
+ * > {@link InstantiationAwareBeanPostProcessor}
+ * <p>
+ * > {@link BeanPostProcessor}
+ * <ul>
+ * <li>1.</li>
+ * <li></li>
+ * <li></li>
+ * <li></li>
+ * <li></li>
+ * <li></li>
+ * <li></li>
+ * <li></li>
+ * <li></li>
+ * <li></li>
+ * <li></li>
+ * </ul>
  */
 @Target(ElementType.TYPE)
 @Retention(RetentionPolicy.RUNTIME)
@@ -132,6 +86,7 @@ public @interface EnableAspectJAutoProxy {
 	 * Indicate that the proxy should be exposed by the AOP framework as a {@code ThreadLocal}
 	 * for retrieval via the {@link org.springframework.aop.framework.AopContext} class.
 	 * Off by default, i.e. no guarantees that {@code AopContext} access will work.
+	 *
 	 * @since 4.3.1
 	 */
 	boolean exposeProxy() default false;
